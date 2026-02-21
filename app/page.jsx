75765b52982c4119fd1057a6624f815e4bdae20e
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-21 17:18:02 +0530

    feat: create cleark auth signup and signin page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,14 @@
+// import { Button } from "@/components/ui/button";
+
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h2>Hello i am priyansh saxena</h2>
-      <Button>Button</Button>
+      {/* <Button>Button</Button> */}
+      {/* <Button>button</Button> */}
+      <Button>sdf</Button>
     </div>
   );
 }
