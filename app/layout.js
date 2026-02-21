726dda365161f42df420a497bc8a1df0f9b27736
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-21 17:49:09 +0530

    feat: create the provider to save user data into db when first login

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-import {Outfit} from "next/font/google";
+import { Outfit } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import Provider from "./provider";
 
 export const metadata = {
   title: "Create Next App",
@@ -17,13 +17,16 @@ const outfit = Outfit({
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-    <html lang="en">
-      <body
-        className={outfit.className}
-      >
-        {children}
-      </body>
-    </html>
+      <html lang="en">
+        <body
+          className={outfit.className}
+        >
+          <Provider>
+            {children}
+          </Provider>
+
+        </body>
+      </html>
     </ClerkProvider>
   );
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,12 @@
-// import { Button } from "@/components/ui/button";
-
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>Hello i am priyansh saxena</h2>
-      {/* <Button>Button</Button> */}
-      {/* <Button>button</Button> */}
       <Button>sdf</Button>
+      <UserButton/>
     </div>
   );
 }
