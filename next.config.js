b15d25bda484c12b44bbe589ff83a002737b50c4
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-22 13:35:48 +0530

    fix: correct the seeting for deployment

--- next.config.js
+++ next.config.js
@@ -2,13 +2,8 @@ const path = require('path');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': path.resolve(__dirname, './'), // or path.resolve(__dirname, 'src') if you use a src folder
-    };
-    return config;
-  },
+  turbopack: {}
 };
+
 
 module.exports = nextConfig;
