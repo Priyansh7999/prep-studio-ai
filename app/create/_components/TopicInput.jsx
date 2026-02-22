ded88a213b608179240a695fc22edd91ba94dd7a
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-02-22 13:14:37 +0530

    feat: add TopicInput component with topic textarea and difficulty selector

---
+++ app/create/_components/TopicInput.jsx
@@ -0,0 +1,38 @@
+'use client'
+import React, { useState } from 'react'
+import { Textarea } from '@/components/ui/textarea'
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+export default function TopicInput({setTopic,setDifficulty}) {
+    return (
+        <div className='mt-5'>
+            <div className='flex flex-col gap-3'>
+                <h2 className='text-center mb-2 text-lg'>Enter Topic for which you want to generate study material</h2>
+                <Textarea placeholder='Start writing here...' onChange={(e) => setTopic(e.target.value)} />
+            </div>
+            <div className='flex flex-col gap-3 mt-10'>
+                <h2 className='text-center mb-2 text-lg'>Select the difficulty level</h2>
+                <Select onValueChange={(value) => setDifficulty(value)}>
+                    <SelectTrigger className="w-full">
+                        <SelectValue placeholder="Select a difficulty level" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectGroup>
+                            <SelectLabel>Difficulty Level</SelectLabel>
+                            <SelectItem value="Easy">Easy</SelectItem>
+                            <SelectItem value="Medium">Medium</SelectItem>
+                            <SelectItem value="Hard">Hard</SelectItem>
+                        </SelectGroup>
+                    </SelectContent>
+                </Select>
+            </div>
+        </div>
+    )
+}
