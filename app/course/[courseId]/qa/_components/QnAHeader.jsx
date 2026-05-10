1ae71ddd82a50152aa93e1faeff4f84a47d9f88e
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-05-11 02:23:29 +0530

    feat: crate qa header component

---
+++ app/course/[courseId]/qa/_components/QnAHeader.jsx
@@ -0,0 +1,15 @@
+function QnAHeader() {
+  return (
+    <div className="mb-10">
+      <h1 className="text-4xl font-bold">
+        Question & Answer
+      </h1>
+
+      <p className="text-gray-500 mt-3 text-lg">
+        Practice important interview and conceptual questions.
+      </p>
+    </div>
+  );
+}
+
+export default QnAHeader;
