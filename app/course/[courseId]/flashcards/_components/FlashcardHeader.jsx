7ba49af93db451ef4fb950a7c94410c1ee1ff93a
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-05-11 02:11:13 +0530

    feat: create flash card header component

---
+++ app/course/[courseId]/flashcards/_components/FlashcardHeader.jsx
@@ -0,0 +1,14 @@
+function FlashcardHeader() {
+  return (
+    <div className="mb-10">
+      <h1 className="text-4xl font-bold">
+        Flashcards
+      </h1>
+      <p className="text-gray-500 mt-3 text-lg">
+        Flip cards to quickly revise important concepts.
+      </p>
+    </div>
+  );
+}
+
+export default FlashcardHeader;
