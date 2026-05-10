728f1abc0ad5542bd95aeee0f936a9deffac21a5
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-05-11 01:41:31 +0530

    feat: add course layout page

--- app/course/[courseId]/page.jsx
+++ app/course/[courseId]/page.jsx
@@ -8,20 +8,31 @@ import StudyMaterialSection from './_component/StudyMaterialSection';
 import ChapterList from './_component/ChapterList';
 import Link from 'next/link';
 import { ArrowBigLeft, ArrowLeft } from 'lucide-react';
+import Loading from '@/app/_components/Loading';
 function Course() {
   const { courseId } = useParams();
   const [course, setCourse] = useState();
+  const [loading, setLoading] = useState(false);
   useEffect(() => {
     GetCourse();
   }, []);
   const GetCourse = async () => {
-    const result = await axios.get("/api/courses?courseId=" + courseId);
-    setCourse(result.data.result);
+    try {
+      setLoading(true);
+      const result = await axios.get("/api/courses?courseId=" + courseId);
+      setCourse(result.data.result);
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+      setLoading(false);
+    }
   };
+  if(loading){
+    return <Loading />
+  }
   return (
     <div>
       <div>
-        <Link href="/dashboard" className='text-sm text-gray-500 flex w-fit mb-1 gap-2 p-2 border border-card-foreground rounded-lg'><ArrowLeft />Back</Link>
         <CourseIntroCard course={course} />
         <StudyMaterialSection courseId={courseId} course={course} />
         <ChapterList course={course} />
