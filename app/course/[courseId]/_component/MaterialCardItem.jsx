2f153b12883366978c9cc7bb1ec3ff6ca05bdb5b
Priyansh Saxena <priyanshsaxena7999@gmail.com>
2026-05-11 02:05:03 +0530

    feat: create study material card item for each course

--- app/course/[courseId]/_component/MaterialCardItem.jsx
+++ app/course/[courseId]/_component/MaterialCardItem.jsx
@@ -1,16 +1,58 @@
+"use client";
 import Image from 'next/image'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from '@/components/ui/button'
-function MaterialCardItem({ item, studyTypeContent }) {
-    console.log("studyTypeContent in MaterialCardItem:", studyTypeContent);
-    return (
-        <div
-            className={`border shadow-md rounded-lg p-5 flex flex-col justify-between items-center
-                ${studyTypeContent?.[item.type]?.length == null && "grayscale"}
-                `}
-        >
+import { generateStudyTypeContent } from '@/lib/generateNotes';
+import axios from 'axios';
+import { RefreshCcw } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
+function MaterialCardItem({ item, studyTypeContent, course, refreshData }) {
+    const [loading, setLoading] = useState(false);
+    const router = useRouter();
 
-            {studyTypeContent?.[item.type]?.length == null ? (
+    const handleRoute = (path) => {
+        router.push(path);
+    }
+
+    const isContentReady = () => {
+        if (!studyTypeContent) return false;
+
+        const content = studyTypeContent[item.type.toLowerCase()];
+        if (!content) return false;
+        return content.length > 0;
+    };
+
+    const GenerateContent = async () => {
+        try {
+            setLoading(true);
+            toast.loading("Generating content...");
+            let chapters = "";
+            course?.courseLayout?.chapters?.forEach((chapter) => {
+                chapters += `${chapter.chapterTitle}, `;
+            });
+
+            await axios.post("/api/generate-study-type-content",{
+                    courseId: course?.courseId,
+                    chapters,
+                    type: item.type,
+                }
+            );
+            refreshData(true);
+            toast.dismiss();
+            toast.success("Content generated successfully");
+        } catch (error) {
+            console.error(error);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const contentReady = isContentReady();
+    return (
+        <div className={`border shadow-md rounded-lg p-5 flex flex-col justify-between items-center
+                ${!contentReady && studyTypeContent?.[item.type]?.length == null && "grayscale"}`}>
+            {!contentReady ? (
                 <h2 className="p-1 px-2 bg-gray-500 text-white rounded-full text-[10px] mb-2">
                     Generate
                 </h2>
@@ -22,15 +64,19 @@ function MaterialCardItem({ item, studyTypeContent }) {
             <Image src={item.icon} alt={item.name} width={50} height={50} />
             <h2 className="font-medium mt-3">{item.name}</h2>
             <p className="text-gray-500 text-sm text-center">{item.desc}</p>
-            {studyTypeContent?.[item.type]?.length == null ? (
+            {!contentReady ? (
                 <Button
                     className="mt-3 w-full"
                     variant="outline"
+                    onClick={GenerateContent}
                 >
+                    {loading && <RefreshCcw className="animate-spin" />}
                     Generate
                 </Button>
             ) : (
-                <Button className="mt-3 w-full" variant="outline">
+                <Button className="mt-3 w-full" variant="outline"
+                    onClick={() => handleRoute(`/course/${course?.courseId}/${item.path}`)} key={item.path}
+                >
                     View
                 </Button>
             )}
